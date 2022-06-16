5f216f5522853b68c7219730ef0f4a72af123d1a
Sudipta <hisudipta26@gmail.com>
2022-06-16 14:55:53 +0530

    organizing components in folders

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpenseItem(props){
    return(
@@ -8,7 +8,7 @@ function ExpenseItem(props){
          <ExpenseDate date={props.date} />
           <div className="expense-item__description">
             <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
+            <div className="expense-item__price">RS. {props.amount}</div>
           </div>
       </Card>
    ) 
