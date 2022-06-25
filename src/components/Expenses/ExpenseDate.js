c689375a824ac52c489711d44eb3950edb708fa8
Sudipta <hisudipta26@gmail.com>
2022-06-25 21:33:10 +0530

    filtered by year

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './ExpenseDate.css';
+
 function ExpenseDate(props){
     const day = props.date.toLocaleString('en-us',{day:'2-digit'});
     const month = props.date.toLocaleString('en-us',{month:'long'});
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
@@ -10,19 +11,31 @@ function Expenses(props) {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = props.expenseData.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
+    let expensesContent = <p className='message'>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+        <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+        />
+        ));
+    }
     
     return(
         <div>
             <Card className='expenses'>
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />   
-                    {props.expenseData.map(expense => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                    ))}   
+                <ExpensesFilter 
+                    selected={filteredYear} 
+                    onChangeFilter={filterChangeHandler} /> 
+                {expensesContent}      
             </Card>
         </div>
     )
