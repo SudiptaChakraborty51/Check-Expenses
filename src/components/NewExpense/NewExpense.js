ee1a156105390c07abd699ed5b7b0b57bf1a872e
Sudipta <hisudipta26@gmail.com>
2022-06-26 01:59:58 +0530

    form updated

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 function NewExpense(props) {
+    const [isAdding, setIsAdding] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsAdding(false);
     };
+
+    const startAddingHandler = () => {
+        setIsAdding(true);
+    }
+
+    const stopAddingHandler = () => {
+        setIsAdding(false);
+    }
 
     return(
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isAdding && (
+                <button onClick={startAddingHandler}>Add New Expenses</button>
+            )}
+            {isAdding && (
+                <ExpenseForm 
+                    onSaveExpenseData={saveExpenseDataHandler} 
+                    onCancel={stopAddingHandler}
+                />
+            )}
         </div>
     )
 }
